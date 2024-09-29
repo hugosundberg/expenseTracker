dba6c8d7feb8da322fa9923020acdda4b92028a6
hugosundberg <hugosundberg98@gmail.com>
2024-09-29 13:44:17 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ExpenseList from "./components/ExpenseList/ExpenseList";
+
+function App() {
+  return (
+    <>
+      <div>EXPENSE TRACKER</div>
+      <ExpenseList />
+    </>
+  );
+}
+
+export default App;
