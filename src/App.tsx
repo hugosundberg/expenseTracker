f2bdafd6c5094922ff62a16c3c27ae5ea9d97226
hugosundberg <hugosundberg98@gmail.com>
2024-09-29 14:13:33 +0200

    form added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import ExpenseList from "./components/ExpenseList/ExpenseList";
+import Form from "./components/Form/Form";
 
 function App() {
   return (
     <>
       <div>EXPENSE TRACKER</div>
+      <Form></Form>
       <ExpenseList />
     </>
   );
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,10 +1,21 @@
-import React from "react";
-
 const Form = () => {
   return (
-    <Form>
-      <input type="text" />
-    </Form>
+    <form>
+      <div className="mb-3">
+        <label htmlFor="description" className="form-label">
+          Description
+        </label>
+        <input id="description" type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="amount" className="form-label">
+          Amount
+        </label>
+        <input id="amount" type="number" className="form-control" />
+      </div>
+
+      <button>Submit</button>
+    </form>
   );
 };
 
